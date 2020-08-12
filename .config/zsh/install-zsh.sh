a9ec8a4c7a34046d1c320f8ab1137589059b461b
Mathias Maes <mathiasmaesdev@gmail.com>
2020-08-12 12:33:16 +0000

    Update install-zsh.sh

--- .config/zsh/install-zsh.sh
+++ .config/zsh/install-zsh.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+zsh -c 'git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"''
+zsh -c 'ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"'
 wget https://gitlab.com/WatcherWhale/dots/-/raw/master/.config/zsh/spaceship.sh
 
-echo -e "source $HOME/spaceship" >> $HOME/.zshrc
+echo source $HOME/spaceship.sh >> $HOME/.zshrc
