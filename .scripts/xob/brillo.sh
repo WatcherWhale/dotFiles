958f724d97d38d93bd2cdc5eb9f1bc26a2f9f272
Mathias Maes <mathiasmaesdev@gmail.com>
2020-09-07 19:29:40 +0200

    Back to brillo

--- .scripts/xob/brillo.sh
+++ .scripts/xob/brillo.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 if [ "$1" = "dim" ]; then
-    xbacklight -set 1
+    brillo -S 1
 elif [ "$1" = "bright" ]; then
-    xbacklight -set 100
+    brillo -S 100
 else
     negative=$(( $1 < 0 ))
 
     if [ $negative -eq 1 ]; then
         let bright=-1*$1
-        xbacklight -dec $bright
+        brillo -U $bright
     else
-        xbacklight -inc $1
+        brillo -A $1
     fi
 fi
 
-xbacklight -get > /tmp/brillo
+brillo > /tmp/brillo
